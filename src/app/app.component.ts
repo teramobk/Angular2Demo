98b227f099e158e4d4837abf69358c3f315c541c
teramobk <teramobk@co.larimer.co.us>
2017-01-13 14:42:52 -0700

    finished with the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero, HEROES } from './hero'
 
+import { HeroService } from './hero.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private heroService: HeroService) {
+    this.heroService.getHeroes().then(results => this.heroes = results);
+  }
   title = 'Tour Of Heroes';
   selectedHero: Hero;
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   select(hero: Hero): void {
     this.selectedHero = hero;
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,5 +13,18 @@ export class HeroService {
   constructor(private http: Http) { }
 
   heroesUrl : string = 'http://vard12/dataservices/WebApi/Angular2Demo/api/heroes';
+getHeroes(): Promise<Hero[]> {
+  var list = this.http.get(this.heroesUrl)
+  .toPromise()
+  .then(response => response.json() as Hero[])
+  .catch(this.handleError);
+
+  return list;
+}
+
+private handleError(error: any): Promise<any>{
+  console.error('Error:', error);
+  return Promise.reject(error.message || error);
+}
 
 }
